d048fcd1d3f294df2aa2e565ab670144b86b4aca
BongMin <ehvkan1@gmail.com>
2022-11-24 16:59:20 +0900

    c++ 11

---
+++ C++/11주차 수업/오버라이딩과 가상 함수 호출.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Base {
+public:
+	virtual void f() { cout << "Base::f() called" << endl; }
+};
+
+class Derived : public Base {
+public:
+	virtual void f() { cout << "Derived::f() called" << endl; } 
+};
+
+void main()
+{
+	Derived d, * pDer;
+	pDer = &d; // 객체 d를 가리킨다.
+	pDer->f(); // Derived의 멤버 f() 호출
+	 
+	Base* pBase;
+	pBase = pDer; // 업캐스팅. 객체 d를 가리킨다.
+	pBase->f(); // 동적 바인딩 발생!! Derived::f() 실행
+}
